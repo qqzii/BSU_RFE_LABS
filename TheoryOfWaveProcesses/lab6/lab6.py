3fb054b639f53fbc6fc31edc969b31c18b2c4c0f
Artemy <artemiy.morozov.00@mail.ru>
2021-11-12 02:00:38 +0300

    12.11.21

--- TheoryOfWaveProcesses/lab6/lab6.py
+++ TheoryOfWaveProcesses/lab6/lab6.py
@@ -1,1 +1,33 @@
-# )
+from functions import graph_add
+
+import pandas
+from math import cos, sin
+
+
+data = pandas.read_csv('measurements/frequency_2500MHz.csv')
+angle = data['angle']
+u_max = data['U_max']
+u_min = data['U_min']
+
+
+x = []
+y = []
+for i in angle:
+    x.append(sin(i))
+    y.append(cos(i))
+
+
+pf = 0.785
+df = -1.571
+a = cos(pf)
+b = sin(pf)
+
+px = []
+for u in u_max:
+    px.append(a * cos(u))
+
+py = []
+for u in u_max:
+    py.append(b * cos(u - df))
+
+graph_add(px, py, ['graph1', '.', '.', '.'])
